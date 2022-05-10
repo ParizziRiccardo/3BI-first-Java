7d50b735515a75be52f57ed13e6b64cd5b896bc5
Parizzi Riccardo <parizzi.riccardo@osdbsesto>
2022-05-10 13:21:48 +0200

    init repo

---
+++ 3BI-first-java/src/pkg3bi/first/java/FirstJava.java
@@ -0,0 +1,16 @@
+package pkg3bi.first.java;
+
+/**
+ *
+ * @author parizzi.riccardo
+ */
+public class FirstJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
